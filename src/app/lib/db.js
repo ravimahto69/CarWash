172359c530c93869b34e24b2489f238b096996cc
ravimahto69 <krravi073@gmail.com>
2026-01-03 11:31:14 +0530

    connect to the database

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,2 +1,30 @@
 import mongoose from 'mongoose'
 
+const MONGO_URL = process.env.MONGO_DB_URL
+
+if(!MONGO_URL){
+    throw new Error("Please check the database enviroment variable")
+}
+let cached = global.mongoose
+
+if(!chached){
+    cached = mongoose.connect(MONGO_URL)
+}
+
+export async function dbConnection(){
+    if(cached.conn) return cached.conn
+
+    if(!cached.promise){
+        cached.promise = mongoose.connect(MONGO_URL,{
+            bufferCommands:false,
+        }).then((mongoose)=>{
+            console.log("connected to database");
+            
+            return mongoose
+        })
+    }
+    cached.conn = await cached.promise
+    return cached.conn
+}
+
+export default dbConnection
